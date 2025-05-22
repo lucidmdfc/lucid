85fa95a22adba5153af3db47107623964864a6a7
= <=>
2025-05-22 12:29:30 +0100

    Disable the Undici integration (fix for TypeError on headers)

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -2,14 +2,18 @@
 // The config you add here will be used whenever the server handles a request.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from "@sentry/nextjs";
+import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
-  dsn: "https://810659bed2867e052139c160c4f3ffc6@o4506791202717696.ingest.sentry.io/4506791204683776",
+  dsn: 'https://810659bed2867e052139c160c4f3ffc6@o4506791202717696.ingest.sentry.io/4506791204683776',
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
+  // Disable the Undici integration (fix for TypeError on headers)
+  integrations(integrations) {
+    return integrations.filter((integration) => integration.name !== 'Undici');
+  },
 });
